222d14a848903a0f78d3a5b8ca92bfc71a08d41f
Benji <benji@benjicraft.info>
2017-06-09 22:20:56 +0100

    Merged from private repo

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jamin_rules
+ * Date: 09/06/2017
+ * Time: 21:17
+ */
+function bathroom()
+{
+    $db_conx = mysqli_connect($host, $user, $password, $schema, $port);
+    $bathroom = mysqli_query($db_conx, 'SELECT status FROM statuses');
+    $row = mysqli_fetch_assoc($bathroom);
+    $br = $row['status'];
+    echo "Bathroom: <br \>";
+    if ($br == 1) {
+        echo('<img src="res/Green.png">');
+    } elseif ($br == 2) {
+        echo('<img src="res/Yellow.png">');
+    } elseif ($br == 3) {
+        echo('<img src="res/Red.png">');
+    } else {
+        echo('<img src="res/unknown.png">');
+    }
+}
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Panel</title>
+    <!-- Bootstrap Links -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <?php include "config.php"?>
+</head>
+<body>
+<h1>StrapBoard</h1>
+<h2>Check on everything from here!</h2>
+<p class="centre">Have a problem? Email: <a href="mailto:strapboard@benjisoft.org.uk">panel@benjisoft.org.uk</a></p>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benji
+ * Date: 07/05/2017
+ * Time: 20:12
+ */
+
+?>
+</body>
+</html>
