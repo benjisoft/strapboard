f828d10a20b836cbf45e351eb3bb79ca39703b38
Benji <benji@benjicraft.info>
2017-07-29 22:43:10 +0100

    Added basic admin pages

--- admin/settings/general.php
+++ admin/settings/general.php
@@ -1,7 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php
+    $sitename = 'StrapBoard';
+    $admintitle = 'Admin Console | '.$sitename;
+    echo '<title>'.$admintitle.'</title>';
+    include '../lib/bootstrap.php';
+    ?>
+    <link rel="stylesheet" type="text/css" href="css/admin.css">
+</head>
+<body>
 <?php
-/**
- * Created by PhpStorm.
- * User: jamin_rules
- * Date: 29/07/2017
- * Time: 21:19
- */
+include 'lib/menu.php';
+?>
+</body>
