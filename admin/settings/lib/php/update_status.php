0bfb5fe9eb9b20a119876b035fc4a475349e564a
Benji <benji@benjisoft.org.uk>
2017-08-18 10:02:22 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- admin/settings/lib/php/update_status.php
+++ admin/settings/lib/php/update_status.php
@@ -3,9 +3,8 @@
  * Created by PhpStorm.
  * User: jamin_rules
  * Date: 18/08/2017
- * Time: 10:23
+ * Time: 10:23.
  */
-
 include '../../../../config.php';
 
 $status = $_GET['status'];
@@ -15,16 +14,15 @@ $id = $_GET['id'];
 $conn = mysqli_connect($host, $user, $pass, $schema);
 // Check connection
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die('Connection failed: '.mysqli_connect_error());
 }
 
 $sql = "UPDATE statuses SET status=$status WHERE id=$id";
 
 if (mysqli_query($conn, $sql)) {
-    echo "Record updated successfully <a href=\"javascript:history.back()\">Go Back</a>";
+    echo 'Record updated successfully <a href="javascript:history.back()">Go Back</a>';
 } else {
-    echo "Error updating record: " . mysqli_error($conn);
+    echo 'Error updating record: '.mysqli_error($conn);
 }
 
 mysqli_close($conn);
-
